2947e9794c89060e5aac346a0549fb18a2c48c3e
linrium <linrium@gmail.com>
2018-04-04 15:08:54 +0700

    add rotate matrix

--- src/umbala-client/src/router.js
+++ src/umbala-client/src/router.js
@@ -1,22 +1,26 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import About from './views/About.vue'
+import Home from './views/Home.vue'
+import Test2 from './views/Test2.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
 	mode: 'history',
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About
-    }
-  ]
-});
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: Home
+		}, {
+			path: '/test2',
+			name: 'test2',
+			component: Test2
+		}, {
+			path: '/about',
+			name: 'about',
+			component: About
+		}
+	]
+})
---
+++ src/umbala-client/src/views/Test2.vue
@@ -0,0 +1,54 @@
+<template>
+	<div>
+		<h1>Rotate Matrix</h1>
+		<input type="number" placeholder="Enter n" v-model="n">
+		<button @click="fillMatrix">Create matrix</button>
+		<button @click="rotate90deg">Rotate matrix</button>
+
+		<table style="margin: 20px auto">
+			<tr v-for="(row, index) in matrix" :key="index">
+				<td v-for="val in row" :key="val">{{val}}</td>
+			</tr>
+		</table>
+
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			n: 5,
+			matrix: []
+		}
+	},
+	methods: {
+		createMatrix() {
+			return Array.from({
+				length: this.n
+			}, () => new Array(this.n).fill(0))
+		},
+		fillMatrix() {
+			const tmp = this.createMatrix()
+			let count = 0
+			for(let i = 0; i < this.n; i++) {
+				for(let j = 0; j < this.n; j++) {
+					count++
+					tmp[i][j] = count
+				}
+			}
+			this.matrix = tmp
+		},
+		rotate90deg() {
+			this.matrix = this.matrix.reverse()
+			for(let i = 0; i < this.matrix.length; i++) {
+				for(let j = 0; j < i; j++) {
+					const top = this.matrix[i][j]
+					this.matrix[i][j] = this.matrix[j][i]
+					this.matrix[j][i] = top
+				}
+			}
+		}
+	}
+}
+</script>
